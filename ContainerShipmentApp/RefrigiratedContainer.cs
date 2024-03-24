18fde1a7f5af8bb87c9c1430c40e07065fb2e74a
Filip Szymanski <wafel@Filips-MacBook-Pro.local>
2024-03-24 01:11:51 +0100

    added refrigirated Container

---
+++ ContainerShipmentApp/RefrigiratedContainer.cs
@@ -0,0 +1,43 @@
+namespace ContainerShipmentApp;
+
+public class RefrigiratedContainer
+{
+    public class RefrigeratedContainer : Container, IHazardNotifier
+    {
+        public string ContentType { get; set; }
+        public double RequiredTemperature { get; private set; }
+
+        public RefrigeratedContainer(int height, double weight, int depth, string serialNumber, double maxLoadCapacity, double requiredTemperature)
+            : base(height, weight, depth, serialNumber, maxLoadCapacity)
+        {
+            RequiredTemperature = requiredTemperature;
+        }
+        
+        public void LoadContainer(double mass, string contentType)
+        {
+            if (LoadMass == 0)
+                ContentType = contentType;
+
+            if (mass > MaxLoadCapacity)
+                throw new OverfillException("Mass exceeds MaxLoad");
+            
+            if(contentType == ContentType)
+                NotifyHazard($"Types of contents does not match");
+        
+            LoadMass = mass;
+        }
+
+        public void UnloadContainer()
+        {
+            LoadMass = 0;
+            ContentType = "none";
+        }
+
+
+        public void NotifyHazard(string message)
+        {
+            Console.WriteLine($"Hazard warning for container {SerialNumber}: {message}");
+        }
+        
+    }
+}
